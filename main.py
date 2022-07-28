371cc2d2203e1fd86776349f31f98fce8ec0d772
Bartosz Kobylinski <bartoszkobylinski@Bartoszs-Mac-mini.local>
2022-07-28 19:08:24 +0200

    Adding an integration with Airtable database where you can get lists of sentence attached to users

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 from aiohttp import client 
 import discord
+from pyairtable import Table
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 '''
@@ -13,17 +15,29 @@ class MyClient(discord.Client):
         if message.author == client.user:
             return
         if message.content.startswith('$hello'):
-            await message.channel.send('Hello!')
-            
+            await message.channel.send('Hello!')        
 intents = discord.Intents.default()
 intents.message_content = True
-
-
 server_id = os.getenv("SERVER_ID")
-
 client = MyClient(intents=intents)
-
 client.run(os.getenv("DISCORD_TOKEN"))
+
+'''
+# Airtable integration
+
+def get_users_list_with_sentences():
+    users = []
+    sentence = []
+    table = Table(os.getenv("AIRTABLE_API_KEY"), os.getenv("AIRTABLE_BASE_ID"), os.getenv('AIRTABLE_TABLE_NAME'))
+    data = table.all()
+    for row in data:
+        users.append(row.get('fields','do not find fields').get("User",'do not find user'))
+        sentence.append(row.get('fields','do not find fields').get("Sentences", 'do not find user'))
+    return dict(zip(users,sentence))
+
+users_sentence_list = get_users_list_with_sentences()
+print(users_sentence_list)
+
 
 '''
 intents = discord.Intents.default()
@@ -43,4 +57,4 @@ async def on_message(message):
         await message.channel.send("Hello")
 
 client.run(os.getenv("DISCORD_TOKEN"))
-
+'''
