1df1bf626178d6842dc71575a3a7e794b37b2e1c
Bartosz Kobylinski <bartoszkobylinski@Bartoszs-Mac-mini.local>
2022-08-24 21:32:30 +0200

    adding an endpoint for API v1

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from crypt import methods
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+
+information = [
+    {"chanel_one": "This is message for chanel one"},
+    {"chanel_two": "This is message for chanel two"}
+]
+
 
 @app.route('/', methods= ['GET'])
 def home():
     return "<h1> WELCOME, YOU CAN SEND INFORMATION BY FILLING OUT THIS FORM</h1><p>This site is a prototyp API for interacting with channel on discord by using bots</p>"
 
+@app.route('/api/v1/test_endpoint', methods= ['GET'])
+def api():
+    return jsonify(information)
