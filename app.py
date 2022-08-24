75a7b4747c7a7a985e05454bf86d2adac384a52e
Bartosz Kobylinski <bartoszkobylinski@Bartoszs-Mac-mini.local>
2022-08-24 21:35:18 +0200

    adding error handler when page is not found

--- app.py
+++ app.py
@@ -19,3 +19,7 @@ def home():
 @app.route('/api/v1/test_endpoint', methods= ['GET'])
 def api():
     return jsonify(information)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
