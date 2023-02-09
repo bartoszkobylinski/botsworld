6b00a211d50c4a1fb8e87ddb0e220a0d987aa3eb
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-02-09 08:47:24 +0100

    Adding logging module to track how application work during production

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ from flask import Flask, request, jsonify, url_for, render_template
 from flask_restful import Resource, Api
 import requests
 from discord import SyncWebhook
+import logging
+from datetime import datetime
 
+level = logging.DEBUG
+level_name = logging.getLevelName(level)
+logging.basicConfig(filename="logs.txt", filemode='w', level=logging.DEBUG,
+                    format='%(asctime)s - %(message)s')
 app = Flask(__name__)
 api = Api(app)
 HOST = '0.0.0.0'
 
-DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
+try:
+    DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
+except None as error:
+    logging.CRITICAL("Discord Webhook got None value", + error)
 
 @app.route("/")
 def index():
@@ -23,17 +32,19 @@ class BotEndpoint(Resource):
         return jsonify(response)
 
     def run(self):
-        app.run(host='localhost', port=6000)
+        app.run(host=HOST)
 
     def post(self):
         message = request.get_json()
         if not message:
+            logging.error("Error: There is no request message ")
             return 404, {"message": "There is no request message"}
         try:
             webhook = SyncWebhook.from_url(DISCORD_WEBHOOK)
             webhook.send(content=message)
             return {"message": message}, 200
         except Exception as error:
+            logging.error("Exeption occured", exc_info=True)
             return {"message": error}, 404
 
 
