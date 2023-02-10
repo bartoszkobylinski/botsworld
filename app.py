d8ee434853ae499ae982b30fc9dd9a6b555ba8f7
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-02-10 05:42:34 +0100

    Final Update: Cleaning unnecessary code, removing flask-restfull library

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
 import os
-from flask import Flask, request, jsonify, url_for, render_template
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify, render_template
 from discord import SyncWebhook
-import logging
 from dotenv import load_dotenv
-'''
+
+
 load_dotenv()
-
-level = logging.DEBUG
-level_name = logging.getLevelName(level)
-logging.basicConfig(filename="logs.txt", filemode='w', level=logging.DEBUG,
-                    format='%(asctime)s - %(message)s', force=True)
-'''
 app = Flask(__name__)
-api = Api(app)
-
-
-try:
-    DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
-except None as error:
-    logging.CRITICAL("Discord Webhook got None value", + error)
+DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
 
 @app.route("/")
@@ -39,33 +26,6 @@ def send_json_to_discord_bot():
     webhook = SyncWebhook.from_url(DISCORD_WEBHOOK)
     webhook.send(content=message)
     return jsonify({"message": message})
-
-'''
-class BotEndpoint(Resource):
-    def get(self):
-        message = request.args.get("message")
-        response = {"message": " " + (message if message else "default message")}
-        return jsonify(response)
-
-    def run(self):
-        app.run()
-
-    def post(self):
-        message = request.get_json()
-        if not message:
-            logging.error("Error: There is no request message ")
-            return 404, {"message": "There is no request message"}
-        try:
-            webhook = SyncWebhook.from_url(DISCORD_WEBHOOK)
-            webhook.send(content=message)
-            return {"message": message}, 200
-        except Exception as error:
-            logging.error("Exeption occured", exc_info=True)
-            return {"message": error}, 404
-
-
-api.add_resource(BotEndpoint, '/api/v1/send_json')
-'''
 
 
 if __name__ == "__main__":
