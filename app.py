e4e804fd883d0165da0e0a26e7423bdff7bec8e9
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-02-11 13:14:35 +0100

    Fixed: Removing old endpoint, Adding error handling

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ load_dotenv()
 app = Flask(__name__)
 DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 
-def transform_message_to_string(json_data):
-    data = json.loads(json_data)
-    message = data.get("message", "")
-    return message
 
 @app.route("/")
 def index():
@@ -21,29 +17,18 @@ def index():
 @app.route("/api/v1/send_message")
 def send_message():
     if request.method == "POST":
-        message = request.data()
-        discord_webhook = request.data['webhook']
-        print(type)
-        webhook = SyncWebhook.from_url(discord_webhook)
-        webhook.send(content=message["message"])
-        return render_template("send_message.html")
+        try:
+            message = request.get_json()
+            discord_webhook = message['discord_webhook']
+            webhook = SyncWebhook.from_url(discord_webhook)
+            webhook.send(content=message["message"])
+            return jsonify({"message": "Message sent successfully!"}), 200
+        except KeyError:
+            return jsonify({"error": "Missing 'message' or 'discord_webhook' in JSON payload"}), 400
+        except ValueError:
+            return jsonify({"error": "Invalid Discord webhook URL."}), 400
     else:
         return render_template("send_message.html")
-
-@app.route("/api/v1/send_json", methods=["GET"])
-def query_data():
-    data = request.args.get("message")
-    return jsonify({"data":data})
-
-
-@app.route("/api/v1/send_json", methods=["POST"])
-def send_json_to_discord_bot():
-    message = request.get_json()
-    print(f"this is the type{type(message)} and message: {message}")
-    webhook = SyncWebhook.from_url(DISCORD_WEBHOOK)
-    #message = transform_message_to_string(message)
-    webhook.send(content=message['message'])
-    return jsonify({"message": message})
 
 
 if __name__ == "__main__":
